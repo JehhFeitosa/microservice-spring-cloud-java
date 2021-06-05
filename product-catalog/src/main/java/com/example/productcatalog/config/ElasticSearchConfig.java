ff552e63f28fb1cec31ec0915f9d2be950021a9c
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-06-05 19:11:10 -0300

    Aula 2 add configuracoes

---
+++ product-catalog/src/main/java/com/example/productcatalog/config/ElasticSearchConfig.java
@@ -0,0 +1,38 @@
+package com.example.productcatalog.config;
+
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.EntityMapper;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages= "com.example.productcatalog.repository")
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public RestHighLevelClient elasticsearchClient(){
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200", "localhost:9300")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+
+//    @Bean
+//   // @Override
+//    public EntityMapper entityMapper(){
+//        ElasticsearchEntityMapper entityMapper = new ElasticsearchEntityMapper(elasticsearchMappingContext(),
+//                new DefaultConversionService());
+//        entityMapper.setConversions(elasticsearchCustomConversions());
+//
+//        return entityMapper;
+//
+//
+//    }
+}
